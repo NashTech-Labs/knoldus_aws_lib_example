697cada453973bc88cbd4007dfd458e3f32ae95e
prateekgupta <prateekgupta291@gmail.com>
2023-01-19 13:34:04 +0530

    updated bank account model and created a new one for table

--- src/main/scala/com/knoldus/aws/models/kinesis/BankAccount.scala
+++ src/main/scala/com/knoldus/aws/models/kinesis/BankAccount.scala
@@ -1,7 +1,9 @@
 package com.knoldus.aws.models.kinesis
 
-import play.api.libs.json.{ Format, Json }
+import com.knoldus.dynamodb.models.DynamoItem
+import play.api.libs.json.{Format, JsSuccess, JsValue, Json}
 
+import java.time.Instant
 import java.util.UUID
 
 case class BankAccount(
@@ -10,8 +12,22 @@ case class BankAccount(
   accountType: String,
   securityCode: String,
   balance: Double
-)
+) extends DynamoItem {
+  override def partitionKey: String = accountType
+
+  override def sortKey: String = accountOwner
+
+  override def json: String = Json.stringify(Json.toJson(this))
+
+  override def timestamp: Long = Instant.now().toEpochMilli
+}
 
 object BankAccount {
   implicit val format: Format[BankAccount] = Json.format
+
+  def apply(json: JsValue): Either[String, BankAccount] =
+    json.validate[BankAccount] match {
+      case JsSuccess(user, _) => Right(user)
+      case e => Left(s"Failed to deserialize Question $e")
+    }
 }
