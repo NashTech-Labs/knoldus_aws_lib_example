5fb5b35994b8b1c7c86b02fce6469492866f404b
prateekgupta <prateekgupta291@gmail.com>
2023-02-01 12:57:25 +0530

    added more settings

--- src/main/scala/com/knoldus/aws/bootstrap/RoutesInstantiator.scala
+++ src/main/scala/com/knoldus/aws/bootstrap/RoutesInstantiator.scala
@@ -1,13 +1,14 @@
 package com.knoldus.aws.bootstrap
 
 import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.model.HttpMethods._
 import akka.http.scaladsl.server.RouteConcatenation._
 import ch.megard.akka.http.cors.scaladsl.CorsDirectives.cors
-import ch.megard.akka.http.cors.scaladsl.model.HttpOriginMatcher
+import ch.megard.akka.http.cors.scaladsl.model.{HttpHeaderRange, HttpOriginMatcher}
 import ch.megard.akka.http.cors.scaladsl.settings.CorsSettings
 import com.knoldus.aws.routes.dynamodb.QuestionAPIImpl
-import com.knoldus.aws.routes.kinesis.{ BankAccountEventGeneratorRoutes, BankAccountRoutes }
-import com.knoldus.aws.routes.s3.{ DataMigrationAPIImpl, S3BucketAPIImpl }
+import com.knoldus.aws.routes.kinesis.{BankAccountEventGeneratorRoutes, BankAccountRoutes}
+import com.knoldus.aws.routes.s3.{DataMigrationAPIImpl, S3BucketAPIImpl}
 import com.knoldus.aws.routes.sqs.MessagingAPIImpl
 
 class RoutesInstantiator(
@@ -30,7 +31,14 @@ class RoutesInstantiator(
   private val messagingAPIImplRoutes: MessagingAPIImpl =
     new MessagingAPIImpl(services.messagingService)
 
-  val routes: Route = cors(CorsSettings.defaultSettings.withAllowedOrigins(HttpOriginMatcher.*)) {
+  private val corsSettings = CorsSettings.defaultSettings
+    .withAllowedMethods(scala.collection.immutable.Seq(GET, POST, PUT, DELETE))
+    .withAllowedOrigins(HttpOriginMatcher.*)
+    .withAllowedHeaders(HttpHeaderRange.*)
+    .withAllowCredentials(false)
+    .withAllowGenericHttpRequests(true)
+
+  val routes: Route = cors(corsSettings) {
     concat(
       questionAPIRoutes.routes,
       bankAccountEventRoutes.routes,
